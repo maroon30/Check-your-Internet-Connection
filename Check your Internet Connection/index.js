ac7a468452c1e409d5f727a5326c7ef03e6543f2
maroon30 <75967879+maroon30@users.noreply.github.com>
2021-11-01 07:08:43 +0530

    Added how to check connection of your internet

---
+++ Check your Internet Connection/index.js
@@ -0,0 +1,27 @@
+
+//   @author Suhana
+//   Project name == Dictionary App
+
+//   Cleaning code == Done
+//   Organizing code == Done
+
+
+let message = document.getElementById("message");
+
+    let messageOnline = () => {
+        message.textContent = "Internet Connection Available";
+        message.style.cssText = "background-color: #E7F6D5; color: #689F38";
+    };
+    let messageOffline = () => {
+        message.textContent = "No Internet Connection";
+        message.style.cssText = "background-color: #FFDDE0; color: #D32F2F";
+    };
+
+    if (window.navigator.onLine) {
+        messageOnline();
+    } else {
+        messageOffline();
+    }
+    
+    window.addEventListener("online", messageOnline);
+    window.addEventListener("offline", messageOffline);
